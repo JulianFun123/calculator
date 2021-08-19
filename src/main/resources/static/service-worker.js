42527dfa4ab5b5a89dde9762404a13fe7de4dcfc
Julian Gojani <julian@gojani.xyz>
2021-08-19 16:44:35 +0200

    Fix pwa

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -6,13 +6,14 @@ const ASSETS = [
     "/android-chrome-192x192.png",
     "/login.html",
     "/logged_in.html",
-    "error.html"
+    "/error.html"
 ]
 const CACHE_NAME = "CALC-1.0"
 
 self.addEventListener('install', function(event) {
     console.log('Install!');
 
+    console.log("Caching")
     event.waitUntil(
         caches
             .open(CACHE_NAME)
@@ -28,6 +29,7 @@ self.addEventListener("activate", event => {
 self.addEventListener('fetch', function(event) {
     console.log('Fetch!', event.request);
     if (navigator.onLine === false) {
+        console.log("Loading cache")
         event.respondWith(
             fetch(event.request).catch(err =>
                 self.cache.open(CACHE_NAME).then(cache => cache.match("/error.html"))
