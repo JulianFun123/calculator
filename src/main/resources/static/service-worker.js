036aa8903ca0d12ec73d507c3b98be15671368cd
Julian Gojani <julian@gojani.xyz>
2021-08-19 16:53:27 +0200

    Fix pwa

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -8,32 +8,26 @@ const ASSETS = [
     "/logged_in.html",
     "/error.html"
 ]
-const CACHE_NAME = "CALC-1.0"
+const CACHE_NAME = "CALC-1.1"
 
-self.addEventListener('install', function(event) {
-    console.log('Install!');
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(ASSETS);
+    })());
+});
 
-    console.log("Caching")
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(ASSETS);
-            })
-            .catch(err => console.log(err))
-    );
-});
-self.addEventListener("activate", event => {
-    console.log('Activate!');
-});
-self.addEventListener('fetch', function(event) {
-    console.log('Fetch!', event.request);
-    if (navigator.onLine === false) {
-        console.log("Loading cache")
-        event.respondWith(
-            fetch(event.request).catch(err =>
-                self.cache.open(CACHE_NAME).then(cache => cache.match("/error.html"))
-            )
-        );
-    }
+self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) { return r; }
+        const response = await fetch(e.request);
+        const cache = await caches.open(CACHE_NAME);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
 });
